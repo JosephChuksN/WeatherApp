92028fe98c4a880944b2b5421df8546d476f4b91
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-03-17 19:45:25 +0800

    Added mobile view and error handler

--- script.js
+++ script.js
@@ -6,8 +6,48 @@ const input = document.getElementById('input')
 const myForm = document.getElementById('myForm')
 const apiKey = "a83ef4716f5723812feb26d1470f5714"
 
+//This function get your current weather data
+const getCoord = () => {
+   if("geolocation" in navigator){
+     navigator.geolocation.getCurrentPosition(setPosition, showError)
+   }else{
+     alert("Your browser doesn't support geolocation")
+   }
+}
+
+const setPosition = (position) => {
+
+  let latitude =  position.coords.latitude
+  let longitude = position.coords.longitude
+  getCoordWeather(latitude, longitude)
+}
+
+const showError = (error) =>{
+  alert(`${error.message}`)
+}
 
 
+const getCoordWeather = (latitude, longitude) => {
+  let apiGeo = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+   fetch(apiGeo)
+   .then((response) => response.json())
+   .then((details) => {
+     
+    tempreture.innerHTML = `${details.main.temp}<span class="sym" >&#8451;</span>`
+    cityName.innerHTML = details.name
+ description.innerHTML = details.weather[0].description
+ let iconn = details.weather[0].icon
+ let imageUrll = `http://openweathermap.org/img/wn/${iconn}@2x.png`
+ imgg.innerHTML = `<img class="img" src="${imageUrll}"/>`
+
+
+   })
+}
+
+
+ window.onload = getCoord
+
+//This function gets weather data for your search
 const getWeather = () => {
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`
     
@@ -21,12 +61,20 @@ const getWeather = () => {
      let imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
      imgg.innerHTML = `<img class="img" src="${imageUrl}"/>`
 
-
     })
-
+  .catch(() => {
+    if(!input.value){
+      alert('Please input a city name')
+    }else{
+      alert('City not found')
+    }
+    
+  })
 }
 
 myForm.addEventListener('submit', (e) =>{
     e.preventDefault()
     getWeather()
 })
+
+
